08cc3adb44e90450969d815cc02ad39638ce6a48
proalejo <proalejo.gh@gmail.com>
2019-07-15 11:23:59 -0300

    publicaciones

--- muebles.php
+++ muebles.php
@@ -21,7 +21,7 @@
 
 
       <br>
-      <div class="container-fluid">
+     <div class="container-fluid">
   <div class="container-fluid">
   <div class="row">
     <div class="col-md-4">
@@ -29,25 +29,7 @@
       <dl>
         <dt>
           Description lists
-        </dt>
-        <dd>
-          A description list is perfect for defining terms.
-        </dd>
-        <dt>
-          Euismod
-        </dt>
-        <dd>
-          Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.
-        </dd>
-        <dd>
-          Donec id elit non mi porta gravida at eget metus.
-        </dd>
-        <dt>
-          Malesuada porta
-        </dt>
-        <dd>
-          Etiam porta sem malesuada magna mollis euismod.
-        </dd>
+        </dt>        
         <dt>
           Felis euismod semper eget lacinia
         </dt>
@@ -65,24 +47,7 @@
         <dt>
           Description lists
         </dt>
-        <dd>
-          A description list is perfect for defining terms.
-        </dd>
-        <dt>
-          Euismod
-        </dt>
-        <dd>
-          Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.
-        </dd>
-        <dd>
-          Donec id elit non mi porta gravida at eget metus.
-        </dd>
-        <dt>
-          Malesuada porta
-        </dt>
-        <dd>
-          Etiam porta sem malesuada magna mollis euismod.
-        </dd>
+        
         <dt>
           Felis euismod semper eget lacinia
         </dt>
@@ -100,24 +65,7 @@
         <dt>
           Description lists
         </dt>
-        <dd>
-          A description list is perfect for defining terms.
-        </dd>
-        <dt>
-          Euismod
-        </dt>
-        <dd>
-          Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.
-        </dd>
-        <dd>
-          Donec id elit non mi porta gravida at eget metus.
-        </dd>
-        <dt>
-          Malesuada porta
-        </dt>
-        <dd>
-          Etiam porta sem malesuada magna mollis euismod.
-        </dd>
+        
         <dt>
           Felis euismod semper eget lacinia
         </dt>
