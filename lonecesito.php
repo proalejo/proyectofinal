955ee0531489f7aa1ea7c96ae8528c3832cb1150
proalejo <proalejo.gh@gmail.com>
2019-08-06 14:28:37 -0300

    arreglo de lo necesito

--- lonecesito.php
+++ lonecesito.php
@@ -7,62 +7,48 @@ include("nav.php")
   include_once PATH_HELPERS . '/database_helper.php';
   include_once PATH_DAOS . '/mispublicacionesDAO.php';
   include_once PATH_HELPERS .'/html_helper.php';
-
   ?>
 
 
 <div class="container-fluid mb-5 mt-5">
-	<div class="row">
+	<div class="row justify-content-center align-items-center">
 		<div class="col-md-12">
-			<div class="row">
-				<div class="col-md-6">
-					<img alt="producto" id="prod_image" style="max-height: 200px; max-width: 200px;" src="<?= PATH_FILE . '/' .  $img  ?>" />
-					<dl class="mt-5">
-                                <dt>
-                                  <?= $producto ?>
-                                </dt>           
-                                
-                                <dd class="mt-5">
-                                  <?= $descripcion ?>
-                                </dd>
-                              </dl> 
-				</div>
-				<div class="col-md-6">
-					<form role="form">
-						<div class="form-group">
-							 
-							<label for="exampleInputEmail1">
-								Usuario
-							</label>
-							<input type="text" class="form-control" id="inputtext" />
+				<?php
+					if ( !isset($_SESSION["usuario"]) ){
+					?>
+					<div class="iniciarsesion">
+						<h1>Porfavor Iniciar Sesion</h1>
+					</div>
+						<?php
+						}
+						else {
+						?>
+						<h1>Donacion Exitosa</h1>
+						<p>Porfavor elija el metodo de envio</p>
+						<div class="form-check">
+						  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+						  <label class="form-check-label" for="exampleRadios1">
+						    Retiro En Persona
+						  </label>
 						</div>
-						<div class="form-group">
-							 
-							<label for="exampleInputPassword1">
-								Direccion
-							</label>
-							<input type="text" class="form-control" id="inputtext" />
+						<div class="form-check">
+						  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+						  <label class="form-check-label" for="exampleRadios2">
+						    Envio
+						  </label>
 						</div>
-						<div class="checkbox">
-							<label>
-								<input type="checkbox" /> Acuerdo con el Donante
-							</label>
 
-							<label>
-								<input type="checkbox" /> Retiro en Persona
-							</label>
-							 
-							<label>
-								<input type="checkbox" /> Envio
-							</label>
-						</div> 
-						<button type="submit" class="btn btn-primary">
+							<button type="submit" class="btn btn-primary">
 							Aceptar
+								
+							</button>					
+				<?php }
+				?>
 						
-						</button>
-					</form>
-				</div>
-			</div>
+						
+					
+				
+			
 		</div>
 	</div>
 </div>
